983a983b74c09d3a776c11319672c06dcdd21611
Jamie Sauve <jamiesauve@zohomail.com>
2023-05-10 12:55:20 -0700

    Add code to show the user agent, for testing

--- src/components/message-feed.tsx
+++ src/components/message-feed.tsx
@@ -28,7 +28,13 @@ export const MessageFeed: FC<{messages: MessageInfo[] }> = ({ messages }) => {
               </div>
             ))
           }
+
+          {window.navigator.userAgent}
         </div>
       </div>
   )
 }
+
+// in tauri-mac: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko)
+// in Brave browser on Mac, mobile devtools: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36
+// in Brave browser on Mac: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36
