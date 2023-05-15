8cfa956852359f12944f7cd2cb1a887c3678da8e
Jamie Sauve <jamiesauve@zohomail.com>
2023-05-15 15:59:55 -0700

    Try to fix prod issues
    - add the env.production required by Vite
    - add a service worker

---
+++ src/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sage-cache';
+
+const urlsToCache = [
+  '/components',
+  '/helpers',
+  '/integrations',
+  '/types',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
