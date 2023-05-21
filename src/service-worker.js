953577d09c19090ccffb4d671eaa1cbb7a97a65e
Jamie Sauve <jamiesauve@zohomail.com>
2023-05-21 12:24:31 -0700

    Try to fix the service-worker so it stops throwing errors

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,22 +1,6 @@
-const CACHE_NAME = 'sage-cache';
-
-const urlsToCache = [
-  '/components',
-  '/helpers',
-  '/integrations',
-  '/types',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', function(event) {
+  // Don't cache any assets
+  });
+  self.addEventListener('fetch', function(event) {
+  // Don't respond to any fetch requests
+  });
